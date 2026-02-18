c2f5d36d75261160616620755134317c4c40dbee
Josias Mbogle <josiasmbogle237@gmail.com>
2026-02-18 23:30:12 +0100

    delet double item in my dict

--- bibliotheque/main.py
+++ bibliotheque/main.py
@@ -1,11 +1,13 @@
 bibliotheque = [
     {
+        "id":"1",
         "titre": "1984",
         "auteur": "George Orwell",                              
         "annee": 1949,
         "disponible": True
     },
     {
+        "id":"2",
         "titre": "L'Étranger",
         "auteur": "Albert Camus",
         "annee": 1942,
@@ -23,7 +25,17 @@ def addBook(bibliotheque) :
     if avaible == "oui":
         isavaible = True
 
-    bibliotheque.append({"titre":title,"auteur":author,"annee":year,"disponible":isavaible ,})
+    iLivre =  seachbook(bibliotheque,title)
+    
+    if iLivre == len(bibliotheque):
+        bibliotheque.append({"id": len(bibliotheque) + 1,
+                            "titre":title,
+                            "auteur":author,
+                            "annee":year,
+                            "disponible":isavaible ,
+                            })
+    else:
+        return "ce livre existe deja"
 
     return "votre livre a bien ete ajouter"
 
@@ -42,37 +54,41 @@ def delBOOK (bibliotheque,ilivre) :
 
 def main():
     while True :
-        choice = int(input("choisir une action :\n " \
-                            "1. ajouter un livre\n" \
-                            "2. rechercher un livre\n" \
-                            "3. supprimer un livre\n" \
-                            "4. afficher la bibliotheque\n" \
-                            "5. fermer\n" \
-                            "entrez : "
-        ))
+        try : 
+            choice = int(input("choisir une action :\n " \
+                                "1. ajouter un livre\n" \
+                                "2. rechercher un livre\n" \
+                                "3. supprimer un livre\n" \
+                                "4. afficher la bibliotheque\n" \
+                                "5. fermer\n" \
+                                "entrez : "
+            ))
+       
 
-        if choice == 1 :
-            message = addBook(bibliotheque)
-            print(message)
-        elif choice == 2 :
-            title = input("entrez le titre du libre que vous souhaitez rechercher")
-            ilivre = seachbook(bibliotheque,title)
-            if ilivre == len(bibliotheque):
-                print("votre livre n'hesite pas")
-            else:
-                print(f"{bibliotheque[ilivre]}")
-        elif choice == 3 : 
-            livre = input("entrez le titre du livre que vous souhaitez supprimer")
-            isearch = seachbook(bibliotheque, livre)
-            if isearch == len(bibliotheque):
-                print("votre livre n'est pas dans la bibliotheque")
-            else:
-                message = delBOOK(bibliotheque,isearch)
+            if choice == 1 :
+                message = addBook(bibliotheque)
                 print(message)
-        elif choice == 4 :
-            for livre in bibliotheque:
-                print(livre)
-        else :
-            break
+            elif choice == 2 :
+                title = input("entrez le titre du libre que vous souhaitez rechercher")
+                ilivre = seachbook(bibliotheque,title)
+                if ilivre == len(bibliotheque):
+                    print("votre livre n'hesite pas")
+                else:
+                    print(f"{bibliotheque[ilivre]}")
+            elif choice == 3 : 
+                livre = input("entrez le titre du livre que vous souhaitez supprimer")
+                isearch = seachbook(bibliotheque, livre)
+                if isearch == len(bibliotheque):
+                    print("votre livre n'est pas dans la bibliotheque")
+                else:
+                    message = delBOOK(bibliotheque,isearch)
+                    print(message)
+            elif choice == 4 :
+                for livre in bibliotheque:
+                    print(livre)
+            else :
+                break
+        except ValueError:  
+            print("veuillez entrez des valeur valides")
 
 main()  
